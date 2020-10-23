4e8383c81d7df94d28e60fa7f6f0b35ff7b7be15
Josh_Date <jjdate@live.com>
2020-10-23 11:15:47 -0400

    made final updates

--- js/script.js
+++ js/script.js
@@ -18,38 +18,44 @@ const quotes = [
       quote: "You've gotta dance like there's nobody watching,Love like you'll never be hurt, Sing like there's nobody listening, And live like it's heaven on earth.",
       source: 'William W. Purkey',
       citation: 'Becoming an Invitational Leader',
-      year: '2002'
+      year: '2002',
+      tags: 'Life, Leadership, books, reading'
     },
     {
       quote: 'Never trust anyone who has not brought a book with them.',
       source: 'Lemony Snicket',
       citation: 'Horseradish',
-      year: '2007'
+      year: '2007',
+      tags: 'wit and wisdom, books, reading'
     },
     {
       quote: 'There are times to stay put, and what you want will come to you, and there are times to go out into the world and find such a thing for yourself',
       source: 'Lemony Snicket',
       citation: 'Horseradish',
-      year: '2007'
+      year: '2007',
+      tags: 'wit and wisdom, books, reading'
     },
     {
       quote: 'There is some good in this world, and it’s worth fighting for.',
       source: ' J.R.R. Tolkien',
       citation: 'The Two Towers',
-      year: '2012'
+      year: '2012',
+      tags: 'faramir, lord-of-the-rings, pippin, the-return-of-the-king, the-two-towers'
     },
     {
       quote: 'I wanted you to see what real courage is, instead of getting the idea that courage is a man with a gun in his hand. It’s when you know you’re licked before you begin but you begin anyway and you see it through no matter what. You rarely win, but sometimes you do.',
       source: 'Harper Lee',
       citation: 'To Kill a Mockingbird',
-      year: '1960'
+      year: '1960',
+      tags: 'better-life-empathy, atticus-finch, courage'
     },
 
     {
       quote: 'Don’t underestimate me! I don’t quit and I don’t run.',
       source: 'Naruto',
       citation:'',
-      year:''
+      year:'',
+      tags: 'anime'
     }
 ];
 
@@ -71,27 +77,46 @@ function getRandomQuote(arr){
 // This function prints the new quote to the screen
 const printQuote = () => {
   let randomQuoteObj = getRandomQuote(quotes);
-  let quote = ``
-  let html = `<p class="quote">${randomQuoteObj['quote']}</p>
-              <p class="source">${randomQuoteObj['source']}`
+  let html = `
+      <p class="quote">${randomQuoteObj['quote']}</p>
+      <p class="source">${randomQuoteObj['source']}`
 
-              // If the random quote object has a citation property, concatenate a <span> element with the class "citation" to the HTML string.
-              if(randomQuoteObj['citation']){
-                html += `<span class="citation">${randomQuoteObj['citation']}</span>`
-              } else{
-                html += `<span class="citation"></span></p>`
-              }
+      // If the random quote object has a citation property, concatenate a <span> element with the class "citation" to the HTML string.
+        if(randomQuoteObj['citation']){
+          html += `<span class="citation">${randomQuoteObj['citation']}</span>`
+        } else{
+            html += `<span class="citation"></span></p>`
+        }
               
-              // If the random quote object has a year property, concatenate a <span> element with the class "year" to the HTML string.
-              if(randomQuoteObj['year']){
-                html += `<span class="year">${randomQuoteObj['year']}</span></p>`
-              } else {
-                html += `<span class="year"></span></p>`
-              }
-              return document.getElementById('quote-box').innerHTML = html;
+        // If the random quote object has a year property, concatenate a <span> element with the class "year" to the HTML string.
+        if(randomQuoteObj['year']){
+          html += `<span class="year">${randomQuoteObj['year']}</span></p>`
+        }
+
+        if(randomQuoteObj['tags']){
+          html += `<span class="tags">${randomQuoteObj['tags']}</span></p>`
+        }
+  changeBgColor();
+  return document.getElementById('quote-box').innerHTML = html;
 }
 
 
+// Change Background Color Function. This function updates the background color to a random color.
+
+function changeBgColor(){
+  let randColor = Math.floor(Math.random() * 255);
+  let randColor2 = Math.floor(Math.random() * 255);
+  let randColor3 = Math.floor(Math.random() * 255);
+  let style = `rgb(${randColor}, ${randColor2}, ${randColor3})`;
+  const body = document.querySelector('body');
+  return body.style.backgroundColor = style;
+} 
+
+// Timing function using the setInterval() method. This method will print a new quote to the page.
+function interval(){
+  return setInterval(printQuote, 2500);
+}
+interval();
 
 /***
  * click event listener for the print quote button
